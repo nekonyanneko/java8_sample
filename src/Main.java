ec02ea9c2acf2f2c68504eb0b387f91f40be68b3
ubuntu16.04_pc <morita.d.shun@gmail.com>
2017-02-04 21:57:54 +0900

    this code is words to output Console.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+//This code is words to output Console.
 public class Main {
-
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+        System.out.print("java code is Comming Soon");
+    }
 }
