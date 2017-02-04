b923de853b21a4bfb10f45e6812ea908cdd189a6
ubuntu16.04_pc <morita.d.shun@gmail.com>
2017-02-04 22:26:38 +0900

    this code is sample4.java

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,9 @@ This code is words to output Console.
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println('A');
-        System.out.println("Welcome to Java!");
-        System.out.println(123);
+        System.out.println("Circle riteral is : \\");
+        System.out.println("apostoropy riteral is : \'");
+        System.out.println("eight words is : \000");
+        System.out.println("sixteen words is : \u0000");
     }
 }
