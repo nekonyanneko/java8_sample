bc3540f8b2a03f24d82c2657a292ade15a39b7ea
ubuntu16.04_pc <morita.d.shun@gmail.com>
2017-02-04 22:48:32 +0900

    this code is sample6.java in 3.5

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,11 @@ public class Main {
     public static void main(String[] args) throws IOException{
         System.out.println("整数を入力してください");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        int num = Integer.parseInt(str);
-        System.out.println(num + "が入力されました");
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+        int num1 = Integer.parseInt(str1);
+        int num2 = Integer.parseInt(str2);
+        System.out.println(num1 + "が入力されました");
+        System.out.println(num2 + "が入力されました");
     }
 }
