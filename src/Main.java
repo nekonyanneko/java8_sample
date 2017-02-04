dbcbcec670aa5a831d0e4294b43f6cf11afa5b3d
ubuntu16.04_pc <morita.d.shun@gmail.com>
2017-02-04 22:41:58 +0900

    this code is sample3.java in 3.5

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ This code is words to output Console.
  */
 public class Main {
     public static void main(String[] args) {
-        int num;
-        num = 3;
-        System.out.println("num is:" + num);
-        num = 5;
-        System.out.println("num is repair");
-        System.out.println("num is:" + num);
+        int num1, num2;
+        num1 = 3;
+        System.out.println("num1 is:" + num1);
+        num2 = num1;
+        System.out.println("num2 is enter num1");
+        System.out.println("num2 is:" + num2);
     }
 }
