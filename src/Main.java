48bfdf5fa10f18869624a4570dee2f59409a6532
ubuntu16.04_pc <morita.d.shun@gmail.com>
2017-02-04 22:36:04 +0900

    this code is sample6.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ This code is words to output Console.
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("8進数の101文字は: \101");
-        System.out.println("16進数0061文字は: \u0061");
+        System.out.println("10進数の10は:" + 10 + "です。");
+        System.out.println("8進数の10は:" + 010 + "です。");
+        System.out.println("16進数の10は:" + 0x10 + "です。");
+        System.out.println("16進数のFは:" + 0xF + "です。");
     }
 }
