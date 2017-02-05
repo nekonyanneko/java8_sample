cf5a6bbdae032193631ee2571663b24a9f89f9f0
ubuntu16.04_pc <morita.d.shun@gmail.com>
2017-02-05 10:31:51 +0900

    this code is sample1.java in 13

---
+++ src/Car.java
@@ -0,0 +1,22 @@
+public class Car {
+    private int num;
+    private double gas;
+
+    public Car()
+    {
+        this.num = 0;
+        this.gas = 0;
+        System.out.println("車を作成しました");
+    }
+    public void setCar(int n, double g)
+    {
+        this.num = n;
+        this.gas = g;
+        System.out.println("ナンバーを"+this.num+"にガソリンを"+this.gas+"にしました");
+    }
+    public void show()
+    {
+        System.out.println("車のナンバーは"+this.num);
+        System.out.println("ガソリン量は"+this.gas);
+    }
+}
